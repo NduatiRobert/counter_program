85306cc4472f95269302a58a399fb482a702c33a
NduatiRobert <robertnjuguna01@gmail.com>
2024-01-07 13:42:08 +0300

    finished counter program

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let count = 0;
 
 document.getElementById("decrease").onclick = function() {
-
+ count-=1 
+ document.getElementById("label").innerHTML=count;
 }
 
-document.getElementById("decrease").onclick = function() {
+document.getElementById("reset").onclick = function() {
+    count=0
+    document.getElementById("label").innerHTML=count;
     
 }
 
-document.getElementById("decrease").onclick = function() {
-    
+document.getElementById("increase").onclick = function() {
+
+     count+=1 
+ document.getElementById("label").innerHTML=count;
 }
