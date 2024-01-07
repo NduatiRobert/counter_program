5e01651c2399c638d76dd6217181a3d1b471047d
NduatiRobert <robertnjuguna01@gmail.com>
2024-01-07 13:28:28 +0300

    Adding javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+document.getElementById("decrease").onclick = function() {
+
+}
+
+document.getElementById("decrease").onclick = function() {
+    
+}
+
+document.getElementById("decrease").onclick = function() {
+    
+}
